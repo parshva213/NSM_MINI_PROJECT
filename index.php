0dde7e53c5be2276fa8c2bbdf5aad39c236bd7fb
parshva213 <hshahparshva21@gmail.com>
2025-08-17 11:23:36 +0530

    ui error solve

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<<<<<<< Updated upstream
 <?php
 // Redirect to login page
 header('Location: login.php');
 exit();
-=======
-<?php
-// Redirect to login page
-header('Location: login.php');
-exit();
->>>>>>> Stashed changes
 ?>
